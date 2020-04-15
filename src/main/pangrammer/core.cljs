ccb869bc415a093a82b220d4b44049c55edda09e
Tory S. Anderson <torys.anderson@gmail.com>
2020-04-15 13:41:23 -0600

    cleanup, message

--- src/main/pangrammer/core.cljs
+++ src/main/pangrammer/core.cljs
@@ -2,11 +2,7 @@
   (:require [reagent.core :as r]
             [pangrammer.routes :as routes]))
 
-(defn default-view []
-  [:h1 "This is a default js view!!"])
-
 (defn mount-components []
-  ;(r/render [#'navbar] (.getElementById js/document "nav"))
   (r/render-component [@routes/current-view] (.getElementById js/document "app")))
 
 (defn init! []
